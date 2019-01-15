3942a55cdadf277bf2e8ab15bc31e15e25c44a74
jluzadasjr <jayrl707@Jesuss-MBP.hsd1.ca.comcast.net>
2019-01-14 22:35:22 -0800

    updated

---
+++ assets/javascript/app.js
@@ -0,0 +1,55 @@
+var questions = [
+    {
+        prompt: "Who is the strongest fighter on Dragon Ball Z?\n(a) Gohan\n(b) Vegeta\n(c) Goku\n(d) Balma", 
+
+        answer: "c"
+    },
+    {
+        prompt: "Who is Goku's original teacher?\n(a) Hercule Satan\n(b) Master Roshi\n(c) Whis\n(d) Ox King", 
+
+        answer: "b"
+    },
+    {
+        prompt: "What is the name of Balma's company?\n(a) Apple\n(b) Microsoft\n(c) Tesla\n(d) Capsule Corporation", 
+
+        answer: "d"
+    },
+    {
+        prompt: "Who traveled from the future to save Goku's life?\n(a) Trunks\n(b) Goten\n(c) Vegeta\n(d) Krillin", 
+
+        answer: "a"
+    },
+    {
+        prompt: "What is the magical food called that can restore your health?\n(a) Edemame\n(b) Senzu Bean\n(c) Rice Buns\n(d) Sushi", 
+
+        answer: "b"
+    },
+    {
+        prompt: "How many Dragon Balls are there?\n(a) 2\n(b) 7\n(c) 151\n(d) 3", 
+
+        answer: "b"
+    },
+    {
+        prompt: "Who is the prince of the Saiyan race?\n(a) Majin Buu\n(b) Cell\n(c) Vegeta\n(d) Raditz", 
+
+        answer: "c"
+    },
+    {
+        prompt: "What is the name of the dance that combines two beings through a dance or Potara Earring?\n(a) Fusion Dance\n(b) The Shimmy\n(c) Combo Dance\n(d) Break Dance", 
+
+        answer: "a"
+    },
+]
+
+var score = 0; 
+
+// for(var i=0; i < questions.length; i++) {
+//     var response = window.prompt(questions[i].prompt)
+//     if(response == questions[i].answer) {
+//         score++; 
+//         alert("Correct!"); 
+//         } else {
+//             alert("Wrong!");
+//         }
+// }
+// alert("You got " + score) + "/" + questions.length
